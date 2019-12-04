4088252d62e536b088bbfba50eb9f21640a932a9
Emanuel Papiez <emanuelp@windowslive.com>
2019-12-04 11:55:40 +0000

    updated index.js

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,5 @@
 
-/*
 
-  Firebase Login Details
-
-  email     = cs353groupnine@gmail.com
-  password  = xGroupNine2020?*
-
-
-  https://console.firebase.google.com/project/react-game-website/
-
-
-*/
 
 const functions = require('firebase-functions');
 const app = require('express')();
