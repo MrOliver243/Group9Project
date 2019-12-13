50e4e0ff3dd246da262b38d1394b4c11ee79b64a
cathalm1 <55761607+cathalm1@users.noreply.github.com>
2019-12-13 14:31:42 +0000

    Update dataActions.js
    updated dataActions with get Games

--- client/src/Redux/actions/dataActions.js
+++ client/src/Redux/actions/dataActions.js
@@ -1,17 +1,15 @@
-import {
-  SET_ERRORS,
-  CLEAR_ERRORS
-} from '../types';
+import { SET_ERRORS, CLEAR_ERRORS, SET_GAMES, SET_GAME } from '../types';
 import axios from 'axios';
 
-export const getAllGames = () => (dispatch) => {
-  axios.get('https://europe-west1-react-game-website.cloudfunctions.net/api/getAllGames')
-    .then((result) => {
+export const getUserData = (userHandle) => (dispatch) => {
+  axios
+    .get(`https://europe-west1-react-game-website.cloudfunctions.net/api/user/${userHandle}`)
+    .then((res) => {
       dispatch({
-        payload: result.data
+        payload: res.data
       });
     })
-    .catch((error) => {
+    .catch(() => {
       dispatch({
         payload: null
       });
@@ -22,16 +20,44 @@ export const clearErrors = () => (dispatch) => {
   dispatch({ type: CLEAR_ERRORS });
 };
 
-export const getGame = (gameId) => (dispatch) => {
-  axios
-    .get(`https://europe-west1-react-game-website.cloudfunctions.net/api/getGame/${gameId}`)
+// this function gets all games from the database
+
+export const getAllGames = () => (dispatch) => {
+  axios.get('https://europe-west1-react-game-website.cloudfunctions.net/api/getAllGames')
     .then((result) => {
       dispatch({
+        type: SET_GAMES,
         payload: result.data
       });
     })
     .catch((error) => {
       dispatch({
+        type: SET_GAMES,
+        payload: null
+      });
+    });
+};
+
+// not sure if this function is needed
+
+export const clearErrors = () => (dispatch) => {
+  dispatch({ type: CLEAR_ERRORS });
+};
+
+// this function gets one game from the database
+
+export const getGame = (gameId) => (dispatch) => {
+  axios
+    .get(`https://europe-west1-react-game-website.cloudfunctions.net/api/getGame/${gameId}`)
+    .then((result) => {
+      dispatch({
+        type: SET_GAME,
+        payload: result.data
+      });
+    })
+    .catch((error) => {
+      dispatch({
+        type: SET_GAME,
         payload: null
       });
     });
