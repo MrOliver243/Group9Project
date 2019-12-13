cbeb3c12c947a553028b87c9c987fe30fa89de2f
JasonT2018 <56918091+JasonT2018@users.noreply.github.com>
2019-12-13 10:08:01 +0000

    implements about2

--- client/src/components/pages/about.js
+++ client/src/components/pages/about.js
@@ -3,13 +3,12 @@ import React, { Component } from 'react';
 class About extends Component {
   render() {
     return (
-      <div className='container-fluid'>
+      <div className='container-fluid' className="center">
 
-        <h1>About</h1>
+        <h1 class="display-2" classname="font-weight-bold">About</h1>
 
-        Group 9 About Section
-
-
+        <h2 class="display-5">The Gateway was developed and designed by Group 9 and is a website designed by gamers for gamers.</h2>
+        <h2 >It's a platform that allows users to make reviews of their favourite games so others can see which games are right for them.</h2>
       </div>
     );
   }
