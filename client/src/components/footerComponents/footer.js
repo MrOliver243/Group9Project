274748c757ef429bc2b589fd2dca99a99613a6bd
JasonT2018 <56918091+JasonT2018@users.noreply.github.com>
2019-12-13 10:03:08 +0000

    implements footer2

--- client/src/components/footerComponents/footer.js
+++ client/src/components/footerComponents/footer.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
 
 class Footer extends Component {
   render() {
     return (
+
       <footer>
-        FOOTER
+
+        <li className="center">
+          <Link to  ="/About">About</Link>
+        </li>
+
+
+        <div class="row" >
+        <div class="col"  >Contact us: thegateway@email.ie
+        <p>Lead Admin: 086192999</p>
+        </div>
+        <div class="col" >Placeholder</div>
+        <div class="col" >Placeholder</div>
+        <div class="col" >Placeholder</div>
+
+        </div>
+
       </footer>
+
     );
   }
 }
